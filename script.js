217fc4ee99452eacecb7c210dc513287d51344d8
nastiacheba <anastasiachbtrv@gmail.com>
2023-01-28 23:12:53 +0300

    fixed translations and added gitignore

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.number').textContent = '?';
   document.querySelector('.score').textContent = score;
-  displayMessage('Start guessing...');
+  displayMessage('Начать угадывать...');
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
